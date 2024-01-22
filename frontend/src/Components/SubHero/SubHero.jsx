55792ff2ee3e1d232a12b6850163c1a7740a8a3c
Alvisalc <alvisccy1107@gmail.com>
2024-01-21 21:41:45 -0500

    banner

---
+++ frontend/src/Components/SubHero/SubHero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './SubHero.css'
+import banner from "../Assets/banner-1.jpg"
+
+export const SubHero = () => {
+  return (
+    <div className="subhero-container-box">
+        <div className="subhero-container">
+            <div className="subhero-left">
+                <h2>Discover the chocolate world</h2>
+                <p>Your will find the most delighted sweets here.</p>
+            </div>
+            <div className="subhero-right">
+                <img src={banner} alt="banner" />
+                <p>Right</p>
+            </div>
+        </div>
+    </div>
+  )
+}
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -3,13 +3,14 @@ import { Hero } from '../Components/Hero/Hero'
 import { Popular } from '../Components/Popular/Popular'
 import { NewCollections } from '../Components/NewCollections/NewCollections'
 import { Newsletter } from '../Components/Newsletter/Newsletter'
-import { Footer } from '../Components/Footer/Footer'
+import { SubHero } from '../Components/SubHero/SubHero'
 
 export const Shop = () => {
   return (
     <div>
         <Hero/>
         <Popular/>
+        <SubHero/>
         <NewCollections/>
         <Newsletter/>
     </div>
