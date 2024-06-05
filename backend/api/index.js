fd97d4cf8cd608988f76b6d811a5bee162803459
Alvisalc <alvisccy1107@gmail.com>
2024-06-05 19:26:20 -0400

    vercel api and routes

---
+++ backend/api/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const { connectToDatabase } = require("../utils");
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+connectToDatabase();
+
+app.get("/", (req, res) => {
+  res.send("Express App is Running");
+});
+
+app.use("/api/signup", require('./signup'));
+app.use("/api/login", require('./login'));
+app.use("/api/addtocart", require('./addtocart'));
+app.use("/api/removefromcart", require('./removefromcart'));
+app.use("/api/create-checkout-session", require('./create-checkout-session'));
+
+module.exports = app;
